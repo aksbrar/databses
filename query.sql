028575e00d569701899a7b632d3606cb1725d541
aksbrar <aksbrar34@gmail.com>
2025-07-29 16:36:26 -0400

    numerical where conditions

--- query.sql
+++ query.sql
@@ -3,4 +3,12 @@
 
 
 /** Select columns **/
-SELECT brand, model, price FROM cars;
+-- SELECT brand, model, price FROM cars;
+
+/** Select columns and using where to get specific data that met condition **/
+-- SELECT brand, model, price, color FROM cars
+--     WHERE color = 'black';
+    
+/** Select columns and using where to get specific data that met numerical condition **/
+SELECT brand, model, price, color, condition FROM cars
+  WHERE condition >= 3;
