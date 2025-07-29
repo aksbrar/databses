6427159ce3109a16ddd7e22303f356fd4ce224e6
aksbrar <aksbrar34@gmail.com>
2025-07-29 16:40:26 -0400

    not equal to, use != or <>

--- query.sql
+++ query.sql
@@ -10,5 +10,10 @@
 --     WHERE color = 'black';
     
 /** Select columns and using where to get specific data that met numerical condition **/
-SELECT brand, model, price, color, condition FROM cars
-  WHERE condition >= 3;
+-- SELECT brand, model, price, color, condition FROM cars
+--   WHERE condition >= 3;
+
+/** not equal condition **/
+-- SELECT brand, model, color FROM cars
+  -- WHERE color != 'white'
+  -- WHERE color <> 'white'
